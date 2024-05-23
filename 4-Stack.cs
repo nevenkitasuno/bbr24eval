7ed6d09bfe00f9c577171716a24b0abd4a0fd23b
nevenkitasuno <38082848+nevenkitasuno@users.noreply.github.com>
2024-05-23 20:29:00 +0300

    Add files via upload

---
+++ 4-Stack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class Stack<T>
+    {
+        public LinkedList<T> Elements;
+        public Stack()
+        {
+            // инициализация внутреннего хранилища стека
+            Elements = new LinkedList<T>();
+        }
+
+        public int Size()
+        {	  
+            return Elements.Count;
+        }
+
+        public T Pop()
+        {
+            if (Elements.Count == 0) return default; // null, если стек пустой
+            T val = Elements.Last.Value;
+            Elements.RemoveLast();
+            return val;
+        }
+
+        public void Push(T val)
+        {
+            Elements.AddLast(val);
+        }
+
+        public T Peek()
+        {
+            if (Elements.Count == 0) return default; // null, если стек пустой
+            return Elements.Last.Value;
+        }
+    }
+
+}
