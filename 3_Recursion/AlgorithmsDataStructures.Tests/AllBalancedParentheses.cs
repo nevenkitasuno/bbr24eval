fe2a61302bb49c1244e1e930d7160c016b2fadfe
nevenkitasuno <foo@bar.com>
2024-06-22 15:26:11 +0300

    disable long tests

--- 3_Recursion/AlgorithmsDataStructures.Tests/AllBalancedParentheses.cs
+++ 3_Recursion/AlgorithmsDataStructures.Tests/AllBalancedParentheses.cs
@@ -31,11 +31,12 @@ namespace AlgorithmsDataStructures.Tests
 
             Assert.Equal(14, RecursionFunctions.AllBalancedParenthesis(4).Count);
             Assert.Equal(42, RecursionFunctions.AllBalancedParenthesis(5).Count);
-            Assert.Equal(132, RecursionFunctions.AllBalancedParenthesis(6).Count);
-            Assert.Equal(429, RecursionFunctions.AllBalancedParenthesis(7).Count);
-            Assert.Equal(1430, RecursionFunctions.AllBalancedParenthesis(8).Count);
-            Assert.Equal(4862, RecursionFunctions.AllBalancedParenthesis(9).Count);
-            Assert.Equal(16796, RecursionFunctions.AllBalancedParenthesis(10).Count);
+            // long tests
+            // Assert.Equal(132, RecursionFunctions.AllBalancedParenthesis(6).Count);
+            // Assert.Equal(429, RecursionFunctions.AllBalancedParenthesis(7).Count);
+            // Assert.Equal(1430, RecursionFunctions.AllBalancedParenthesis(8).Count);
+            // Assert.Equal(4862, RecursionFunctions.AllBalancedParenthesis(9).Count);
+            // Assert.Equal(16796, RecursionFunctions.AllBalancedParenthesis(10).Count);
             
             // foreach (string str in RecursionFunctions.AllBalancedParenthesis(1)) Console.WriteLine(str);
             // Console.WriteLine();
