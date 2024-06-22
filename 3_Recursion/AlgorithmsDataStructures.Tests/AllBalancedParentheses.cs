87c2fe4ccdd4e84873be442d6e1c15adb23e4d38
nevenkitasuno <foo@bar.com>
2024-06-22 12:07:18 +0300

    tests: recursion 9

--- 3_Recursion/AlgorithmsDataStructures.Tests/AllBalancedParentheses.cs
+++ 3_Recursion/AlgorithmsDataStructures.Tests/AllBalancedParentheses.cs
@@ -12,19 +12,29 @@ namespace AlgorithmsDataStructures.Tests
         [Fact]
         public void Test1()
         {
-            List<string> answer = RecursionFunctions.AllBalancedParentheses(0);
-            foreach (string str in answer) Console.WriteLine(str);
-            Console.WriteLine();
+            Assert.Empty(RecursionFunctions.AllBalancedParentheses(0));
+
+            List<string> answer1 = RecursionFunctions.AllBalancedParentheses(1);
+            Assert.Equal(answer1, new List<string>{"()"});
+
+            List<string> answer2 = RecursionFunctions.AllBalancedParentheses(2);
+            Assert.Equal(2, answer2.Count);
+            Assert.Contains("(())", answer2);
+            Assert.Contains("()()", answer2);
+
+            List<string> answer3 = RecursionFunctions.AllBalancedParentheses(3);
+            Assert.Equal(5, answer3.Count);
+            Assert.Contains("((()))", answer3);
+            Assert.Contains("(()())", answer3);
+            Assert.Contains("(())()", answer3);
+            Assert.Contains("()(())", answer3);
+            Assert.Contains("()()()", answer3);
+
+            List<string> answer4 = RecursionFunctions.AllBalancedParentheses(4);
+            Assert.Equal(14, answer4.Count);
             
-            foreach (string str in RecursionFunctions.AllBalancedParentheses(1)) Console.WriteLine(str);
-            Console.WriteLine();
-            foreach (string str in RecursionFunctions.AllBalancedParentheses(2)) Console.WriteLine(str);
-            Console.WriteLine();
-            foreach (string str in RecursionFunctions.AllBalancedParentheses(3)) Console.WriteLine(str);
-            Console.WriteLine();
-            foreach (string str in RecursionFunctions.AllBalancedParentheses(4)) Console.WriteLine(str);
-            Console.WriteLine();
-            Assert.True(true);
+            // foreach (string str in RecursionFunctions.AllBalancedParentheses(1)) Console.WriteLine(str);
+            // Console.WriteLine();
         }
     }
 }
