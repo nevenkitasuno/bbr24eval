7ad1697ec00d485f970ee74fd350ea78b92cc723
nevenkitasuno <foo@bar.com>
2024-06-24 05:42:35 +0300

    feat: templates for second DSA course

---
+++ 4_DSA/02 binary search tree/DataStructuresAlgorithms/2-Binary-Search-Tree.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+  public class BSTNode<T>
+  {
+    public int NodeKey; // ключ узла
+    public T NodeValue; // значение в узле
+    public BSTNode<T> Parent; // родитель или null для корня
+    public BSTNode<T> LeftChild; // левый потомок
+    public BSTNode<T> RightChild; // правый потомок	
+	
+    public BSTNode(int key, T val, BSTNode<T> parent)
+    {
+      NodeKey = key;
+      NodeValue = val;
+      Parent = parent;
+      LeftChild = null;
+      RightChild = null;
+    }
+  }
+
+  // промежуточный результат поиска
+  public class BSTFind<T>
+  {
+    // null если в дереве вообще нету узлов
+    public BSTNode<T> Node;
+	
+    // true если узел найден
+    public bool NodeHasKey;
+	
+    // true, если родительскому узлу надо добавить новый левым
+    public bool ToLeft;
+	
+    public BSTFind() { Node = null; }
+  }
+
+  public class BST<T>
+  {
+    BSTNode<T> Root; // корень дерева, или null
+	
+    public BST(BSTNode<T> node)
+    {
+	  Root = node;
+    }
+	
+    public BSTFind<T> FindNodeByKey(int key)
+    {
+      // ищем в дереве узел и сопутствующую информацию по ключу
+      return null;
+    }
+	
+    public bool AddKeyValue(int key, T val)
+    {
+      // добавляем ключ-значение в дерево
+      return false; // если ключ уже есть
+    }
+	
+    public BSTNode<T> FinMinMax(BSTNode<T> FromNode, bool FindMax)
+    {
+      // ищем максимальный/минимальный ключ в поддереве
+     return null;
+    }
+	
+    public bool DeleteNodeByKey(int key)
+    {
+      // удаляем узел по ключу
+      return false; // если узел не найден
+    }
+
+    public int Count()
+    {
+      return 0; // количество узлов в дереве
+    }
+	
+  }
+}  
