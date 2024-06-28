9da963f960286904f85edb97dfaf0f89ec4d306c
nevenkitasuno <foo@bar.com>
2024-06-28 18:21:47 +0300

    feat: DSA S2E5 balancedBST

--- 4_DSA/05 balanced bst/DataStructuresAlgorithms/5-Balanced-BST.cs
+++ 4_DSA/05 balanced bst/DataStructuresAlgorithms/5-Balanced-BST.cs
@@ -12,5 +12,13 @@ namespace AlgorithmsDataStructures2
             tree[0] = a[a.Length/2];
 			return tree;
 		}
+        private static void InternalInsertInTree(int[] tree, int treeSlot, int[] source)
+        {
+            if(treeSlot >= tree.Length) return;
+            int mid = source.Length/2;
+            tree[treeSlot] = source[mid];
+            InternalInsertInTree(tree, treeSlot + 1, source[0..(mid - 1)]);
+            InternalInsertInTree(tree, treeSlot + 2, source[(mid + 1)..]);
+        }
 	}
 }  
