28a9c2a10cf2396f42b2ff297c0d2222a0bffb46
nevenkitasuno <foo@bar.com>
2024-07-09 15:42:02 +0300

    feat: DSA S2E7 heap

--- 4_DSA/07 heap/DataStructuresAlgorithms.Tests/UnitTest1.cs
+++ 4_DSA/07 heap/DataStructuresAlgorithms.Tests/UnitTest1.cs
@@ -1,14 +1,58 @@
-using System;
 using Xunit;
+using System.Collections.Generic;
 
-namespace DataStructuresAlgorithms.Tests
+namespace AlgorithmsDataStructures2.Tests
 {
-    public class UnitTest1
+    public class HeapTests
     {
         [Fact]
-        public void Test1()
+        public void MakeHeap_WithValidInput_ReturnsTrue()
         {
+            int[] inputArray = { 15, 25, 10, 30, 20, 5, 17, 22 };
+            int depth = 2;
+            Heap heap = new Heap();
+            heap.MakeHeap(new int[] {}, depth);
+            bool isFull = false;
+            for (int i = 0; i < inputArray.Length && !isFull; i++)
+                isFull = !heap.Add(inputArray[i]);
 
+            Assert.True(isFull);
+        }
+
+        [Fact]
+        public void MakeHeap_WithArrayInput_ReturnsFalse()
+        {
+            int[] inputArray = { 15, 25, 10, 30, 20, 5 };
+            int depth = 3;
+            Heap heap = new Heap();
+            heap.MakeHeap(inputArray, depth);
+            bool isFull = false;
+
+            Assert.False(isFull);
+        }
+
+        [Fact]
+        public void GetMax_WithValidInput_ReturnsCorrectMax()
+        {
+            int depth = 2;
+            int[] inputArray = { 15, 25, 10, 30, 20, 5 };
+            Heap heap = new Heap();
+            heap.MakeHeap(inputArray, depth);
+
+            int expectedMax = 30;
+            int actualMax = heap.GetMax();
+
+            Assert.Equal(expectedMax, actualMax);
+        }
+
+        [Fact]
+        public void GetMax_WithEmptyHeap_ReturnsCorrectValue()
+        {
+            Heap heap = new Heap();
+            int expectedMax = -1;
+            int actualMax = heap.GetMax();
+
+            Assert.Equal(expectedMax, actualMax);
         }
     }
 }
--- 4_DSA/07 heap/DataStructuresAlgorithms/07-Heap.cs
+++ 4_DSA/07 heap/DataStructuresAlgorithms/07-Heap.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AlgorithmsDataStructures2
 {
@@ -7,28 +8,72 @@ namespace AlgorithmsDataStructures2
 	{
 
 		public int[] HeapArray; // хранит неотрицательные числа-ключи
+		private int _count;
+		private int _heapSize;
 
-		public Heap() { HeapArray = null; }
+		public Heap()
+		{
+			HeapArray = null;
+			_count = 0;
+		}
 
 		public void MakeHeap(int[] a, int depth)
 		{
-			// создаём массив кучи HeapArray из заданного
-			// размер массива выбираем на основе глубины depth
-			// ...
+			_heapSize = PowerOfTwo(depth + 1) - 1;
+			HeapArray = new int[_heapSize];
+			_count = 0;
+			bool isFull = false;
+			for (int i = 0; i < a.Length && !isFull; i++) isFull = !Add(a[i]);
 		}
+		private static int PowerOfTwo(int power) => 1 << power;
 
 		public int GetMax()
 		{
 			// вернуть значение корня и перестроить кучу
-			return -1; // если куча пуста
+			if (_count == 0) return -1; // если куча пуста
+			int maxOfHeap = HeapArray[0];
+			HeapArray[0] = HeapArray[--_count];
+			HeapArray[_count] = 0;
+			InternalDownHeap(0);
+			return maxOfHeap;
 		}
 
+		private void InternalDownHeap(int i)
+		{
+			int max = InternalMaxFromParentAndChildren(i);
+            if (max != i)
+            {
+                InternalSwap(ref HeapArray[i], ref HeapArray[max]);
+                InternalDownHeap(max);
+            }
+		}
+
+		private int InternalMaxFromParentAndChildren(int parent)
+		{
+			int max = parent, leftChild = InternalLeftChild(parent), rightChild = InternalRightChild(parent);
+			if (leftChild < _count && HeapArray[max] < HeapArray[leftChild]) max = leftChild;
+			if (rightChild < _count && HeapArray[max] < HeapArray[rightChild]) max = rightChild;
+			return max;
+		}
+		private int InternalParent(int node) => (node - 1) / 2;
+		private int InternalLeftChild(int node) => node * 2 + 1;
+		private int InternalRightChild(int node) => node * 2 + 2;
 		public bool Add(int key)
 		{
-			// добавляем новый элемент key в кучу и перестраиваем её
-			return false; // если куча вся заполнена
+			if (_count == _heapSize) return false;
+			HeapArray[_count++] = key;
+			for (int i = _count - 1; i != 0 && HeapArray[InternalParent(i)] < HeapArray[i]; i = InternalParent(i))
+				InternalSwap(ref HeapArray[InternalParent(i)], ref HeapArray[i]);
+			return true;
 		}
 
+		private static void InternalSwap<T>(ref T left, ref T right)
+		{
+			T temp;
+			temp = left;
+			left = right;
+			right = temp;
+		}
 	}
 }
 
