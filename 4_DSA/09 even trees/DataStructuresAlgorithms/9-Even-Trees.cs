286685515763a89757b040a4498684145f9fd4af
nevenkitasuno <foo@bar.com>
2024-07-10 19:34:10 +0300

    feat: DSA S2E9 even trees template

---
+++ 4_DSA/09 even trees/DataStructuresAlgorithms/9-Even-Trees.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public class SimpleTreeNode<T>
+    {
+        public T NodeValue;
+        public SimpleTreeNode<T> Parent; // null for root
+        public List<SimpleTreeNode<T>> Children; // null if no children
+        public int Level { get; private set; }
+
+        public SimpleTreeNode(T val, SimpleTreeNode<T> parent)
+        {
+            NodeValue = val;
+            Parent = parent;
+            Children = null;
+            Level = parent == null ? 0 : parent.Level + 1;
+        }
+
+        public List<SimpleTreeNode<T>> GetAllNodes()
+        {
+            List<SimpleTreeNode<T>> nodes = new List<SimpleTreeNode<T>> { this };
+            if (Children == null) return nodes;
+            foreach (SimpleTreeNode<T> child in Children)
+                nodes.AddRange(child.GetAllNodes());
+            return nodes;
+        }
+
+        public List<SimpleTreeNode<T>> FindNodesByValue(T val)
+        {
+            List<SimpleTreeNode<T>> searchResult = new List<SimpleTreeNode<T>>();
+            if (NodeValue.Equals(val)) searchResult.Add(this);
+            if (Children == null) return searchResult;
+            foreach (SimpleTreeNode<T> child in Children)
+                searchResult.AddRange(child.FindNodesByValue(val));
+            return searchResult;
+        }
+
+        public int CountDescendants()
+        {
+            if (Children == null) return 0;
+            int count = Children.Count;
+            foreach (SimpleTreeNode<T> child in Children)
+                count += child.CountDescendants();
+            return count;
+        }
+
+        public int LeafCount()
+        {
+            if (Children == null) return 1;
+            int count = 0;
+            foreach (SimpleTreeNode<T> child in Children)
+                count += child.LeafCount();
+            return count;
+        }
+
+        public void RemoveChild(SimpleTreeNode<T> child)
+        {
+            if (Children == null) return;
+            Children.Remove(child);
+            if (Children.Count == 0) Children = null;
+            child.Level = 0;
+        }
+
+        public void ReplaceParent(SimpleTreeNode<T> newParent) // remove parent if newParent is null
+        {
+            if (Parent != null) Parent.RemoveChild(this);
+            if (newParent != null) newParent.AddChild(this);
+            Parent = newParent;
+            Level = newParent == null ? 0 : newParent.Level + 1;
+        }
+
+        public void AddChild(SimpleTreeNode<T> NewChild)
+        {
+            if (Children == null) Children = new List<SimpleTreeNode<T>>();
+            Children.Add(NewChild);
+            NewChild.Parent = this;
+            NewChild.Level = Level + 1;
+        }
+    }
+
+    public class SimpleTree<T>
+    {
+        public SimpleTreeNode<T> Root;
+
+        public SimpleTree(SimpleTreeNode<T> root)
+        {
+            Root = root;
+        }
+
+        public void AddChild(SimpleTreeNode<T> ParentNode, SimpleTreeNode<T> NewChild)
+        {
+            if (ParentNode != null) ParentNode.AddChild(NewChild);
+        }
+
+        public void DeleteNode(SimpleTreeNode<T> NodeToDelete) => MoveNode(NodeToDelete, null); // make parent null
+        public List<SimpleTreeNode<T>> GetAllNodes() => Root.GetAllNodes();
+
+        public List<SimpleTreeNode<T>> FindNodesByValue(T val) => Root.FindNodesByValue(val);
+
+        public void MoveNode(SimpleTreeNode<T> OriginalNode, SimpleTreeNode<T> NewParent)
+        {
+            if (OriginalNode != null) OriginalNode.ReplaceParent(NewParent);
+        }
+
+        public int Count() => Root.CountDescendants() + 1;
+
+        public int LeafCount() => Root.LeafCount();
+
+        public List<T> EvenTrees()
+        {
+            // ...
+        }
+
+    }
+}
