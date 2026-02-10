59e0648e2224b5111731b7b126def4e2a1d641c5
nevenkitasuno <foo@bar.com>
2026-02-10 21:38:37 +0300

    feat: task 12

---
+++ Part 2/2b/2/12/AssignmentAttempt.cs
@@ -0,0 +1,20 @@
+public abstract class General
+{
+    // ...
+    public static void AssignmentAttempt(
+            ref General target,
+            General source)
+        {
+            target = source as General;
+        }
+}
+
+public abstract class Any : General
+{
+    public static void AssignmentAttempt(
+            ref Any target,
+            General source)
+        {
+            target = source as General;
+        }
+}
